76864015497fdab9f8659437f03761d27995df54
Zebratic <zebraticyt@gmail.com>
2024-04-29 02:00:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { exec } = require('child_process');
 const app = express();
 const PORT = 1337;
 
+// read body data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 
@@ -24,7 +27,7 @@ app.post('/shutdown', (req, res) => {
 // ============================= KVM Commands =============================
 // Boot VMs
 app.post('/bootvm', (req, res) => {
-    var domain = req.query.domain;
+    var domain = req.body.domain;
     res.send(`Booting ${domain} VM...`);
     exec(`sudo virsh start ${domain}`, (err, stdout, stderr) => {
         if (err) {
@@ -32,12 +35,19 @@ app.post('/bootvm', (req, res) => {
             return;
         }
         console.log(stdout);
+        exec(`virt-manager --connect qemu:///system --show-domain-console ${domain}`, (err, stdout, stderr) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log(stdout);
+        });
     });
 });
 
 // Shutdown VMs
 app.post('/shutdownvm', (req, res) => {
-    var domain = req.query.domain;
+    var domain = req.body.domain;
     res.send(`Shutting down ${domain} VM...`);
     exec(`sudo virsh shutdown ${domain}`, (err, stdout, stderr) => {
         if (err) {
